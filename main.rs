318106a04b5b07b8eb9833024b34527c5c813626
sunlife3 <fsrkeye1258@gmail.com>
2024-05-03 14:01:36 +0900

    Instruction Decode implemented

--- main.rs
+++ main.rs
@@ -1,57 +1,96 @@
 mod components;
 
-pub use components::components::{m_adder, m_am_imem};
+pub use components::components::{m_adder, m_am_imem, m_cmp, m_mux};
 
 struct Registers {
+    x1: u32,
     r_pc: u32,
 }
 
-struct Wires {
-    w_npc: u32,
-    w_ir: u32,
-    w_clk: bool,
+struct Flags {
+    // Actually, Variables in this struct are unnnecessary because electric curcuit detects these parameters automatically. 
     clk_rising_edge: bool,
 }
 
-
 fn main() {
-    let mut wires = Wires {
-        w_npc: 0,
-        w_ir: 0,
-        w_clk: false,
-        clk_rising_edge: false,
-    };
     
-    let mut registers = Registers {
+    let mut reg = Registers {
+        x1:3,
         r_pc: 0,
     };
 
-    let test_imm = 4;
+    let mut flags = Flags {
+        clk_rising_edge: false,
+    };
+
+    let mut w_clk: bool = false;
     let mut before_clk: bool = false;
 
-    for i in 0..401 {
+    let test_imm = 4;
+
+    for i in 1..401 {
+        // ===============  Wires ONLY used in single stage =============================
+        // Wires don't memorise, so wires have to be declared as local variable in loop.
+        // (Clock signal is needed all stages, so is declared in out of loop.)
+        let mut w_npc: u32 = 0;
+        let mut w_ir: u32 = m_am_imem(&reg.r_pc); 
+            //Initial instrucrtion is loaded when instruction memory is connected to PC circuit-wise.
+        let mut w_rt: u32 = 0;
+
+        let cmp1;
+        let cmp2;
+        let mut w_r1 = 0;
+        let mut w_r2 = 0;
+        //==============================================================================
+
         if 150 < i {
+            
             if i%50 == 0 { 
                 // Reverse clock 
-                wires.w_clk = !wires.w_clk;
+                w_clk = !w_clk;
             }
-            if before_clk != wires.w_clk && wires.w_clk == true{
+            if before_clk != w_clk && w_clk == true{
                 // Detect rising edge
-                // Rising edge is occured when both of below 2 condition satisfied.
+                // Rising edge is occured when both of below 2 condition are satisfied.
                 //  1 Before clk is different from current clk.
                 //  2 Current clk is high.
-                wires.clk_rising_edge = true;
+                flags.clk_rising_edge = true;
             }else {
-                wires.clk_rising_edge = false;
+                flags.clk_rising_edge = false;
             }
-            if wires.clk_rising_edge {                 
-                wires.w_npc = m_adder(&test_imm, &registers.r_pc);
-                registers.r_pc = wires.w_npc;
-                wires.w_ir = m_am_imem(&registers.r_pc);
+
+            if w_clk {                
+                // Instruction Fetch
+                w_ir = m_am_imem(&reg.r_pc);
+                w_npc = m_adder(&test_imm, &reg.r_pc);
+
+                // Instruction Decode
+                cmp1 = m_cmp(0b00001, (w_ir >> 15) & 0b11111);
+                cmp2 = m_cmp(0b00001, (w_ir >> 20) & 0b11111);
+                w_r1 = m_mux(&0, &reg.x1, &cmp1);
+                w_r2 = m_mux(&0, &reg.x1, &cmp2);
+
+                // Execute
+                w_rt = m_adder(&w_r1, &w_r2);
+            }
+
+            if flags.clk_rising_edge {
+                // Register's value update 
+                reg.r_pc = w_npc;
+                reg.x1 = w_rt;
                 
             }
-            if i%100 ==99{println!("{}, Ox{:08x}, Ox{:08x}",i, registers.r_pc, wires.w_ir);}
-            before_clk = wires.w_clk; 
+
+            if i%99 == 0{
+                // IF test (before rising edge)
+                // println!("{}, Ox{:08x}, 0x{:08x}",i, reg.r_pc, w_ir);
+            }
+            if i%100 == 0{
+                // ID test ()
+                println!("{}, {}, {}, {}", i, w_r1, w_r2, w_rt);
+            }
+            
+            before_clk = w_clk; 
         }
     }    
 }
