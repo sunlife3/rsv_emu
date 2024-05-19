06ed91cb1b9512ec8e0f92760ebfd52d0d3e41f3
sunlife3 <fsrkeye1258@gmail.com>
2024-05-20 00:24:34 +0900

    Register file implemented, ditch pseudo time counter

--- main.rs
+++ main.rs
@@ -1,33 +1,53 @@
 mod components;
-mod variables;
+mod registers;
 mod procs;
 
 pub use components::components::*;
-use variables::{Registers, Flags};
+use registers::{Registers, Flags};
 use procs::proc::proc1;
+
+struct Wires {
+    w_rt: u32,
+    w_ir: u32,
+    w_clk: bool,
+    before_clk: bool,
+} 
+
 
 fn main() {
     
     let mut reg = Registers::new(3,0);
+    let w_ir_init: u32 = m_am_imem(&reg.get_rpc()); 
+    let mut wire = Wires{
+        w_rt: 0,
+        w_ir: 0, 
+        w_clk: false,
+        before_clk: false,
+        
+    };
+    wire.w_ir = w_ir_init;
+
 
     let mut flags = Flags {
         clk_rising_edge: false,
     };
 
-    let mut w_clk: bool = false;
-    let mut before_clk: bool = false;
+    
 
     let test_imm = 4;
 
-    for i in 1..401 {
+    for i in 1..158 {
         // ===============  Wires ONLY used in single stage =============================
         // Wires don't memorise, so wires have to be declared as local variable in loop.
         // (Clock signal is needed all stages, so is declared in out of loop.)
         let rpc = reg.get_rpc();
         let mut w_npc: u32 = 0;
-        let mut w_ir: u32 = m_am_imem(&rpc); 
+        
             //Initial instrucrtion is loaded when instruction memory is connected to PC circuit-wise.
-        let mut w_rt: u32 = 0;
+
+        let mut ra1: usize = 0;
+        let mut  ra2: usize = 0;
+        let mut  wa: usize = 0;
 
         let mut w_r1 = 0;
         let mut w_r2 = 0;
@@ -35,11 +55,9 @@ fn main() {
 
         if 150 < i {
             
-            if i%50 == 0 { 
-                // Reverse clock 
-                w_clk = !w_clk;
-            }
-            if before_clk != w_clk && w_clk == true{
+            wire.w_clk = !wire.w_clk;
+        
+            if wire.before_clk != wire.w_clk && wire.w_clk == true{
                 // Detect rising edge
                 // Rising edge is occured when both of below 2 condition are satisfied.
                 //  1 Before clk is different from current clk.
@@ -49,35 +67,36 @@ fn main() {
                 flags.clk_rising_edge = false;
             }
 
-            if w_clk {                
+            if wire.w_clk {                
                 // Instruction Fetch
-                w_ir = m_am_imem(&rpc);
+                wire.w_ir = m_am_imem(&rpc);
                 w_npc = m_adder(&test_imm, &rpc);
 
                 // Instruction Decode
-                (w_r1, w_r2) = proc1(&reg, &w_ir);
+                //(w_r1, w_r2) = proc1(&reg, &w_ir);
+                ra1 = (wire.w_ir as usize >> 15) & 0b11111;
+                ra2 = (wire.w_ir as usize >> 20) & 0b11111;
+                wa  = (wire.w_ir as usize >> 7) & 0b11111;
+
+                println!("1 [{}] w_rt={} (ra1={}, ra2={})", i, wire.w_rt, ra1, ra2);
+                (w_r1, w_r2) = reg.m_register_file(ra1, ra2, wa, true, &wire.w_rt);
+                println!("2 [{}] w_rt={}", i, wire.w_rt);
 
                 // Execute
-                w_rt = m_adder(&w_r1, &w_r2);
+                wire.w_rt = m_adder(&w_r1, &w_r2);
+                println!("3 [{}] w_rt={} (w_r1={}, w_r2={})\r\n\r\n", i, wire.w_rt, w_r1, w_r2);
+               
             }
 
             if flags.clk_rising_edge {
                 // Register's value update 
                 reg.set_rpc(w_npc);
-                reg.set_x1(w_rt);
-                
+                reg.set_x(wire.w_rt, wa);
             }
 
-            if i%99 == 0{
-                // IF test (before rising edge)
-                // println!("{}, Ox{:08x}, 0x{:08x}",i, reg.r_pc, w_ir);
-            }
-            if i%100 == 0{
-                // ID test ()
-                println!("{}, {}, {}, {}", i, w_r1, w_r2, w_rt);
-            }
+            println!("{}, {}, {}, {}", i, w_r1, w_r2, wire.w_rt);
             
-            before_clk = w_clk; 
+            wire.before_clk = wire.w_clk; 
         }
     }    
 }
